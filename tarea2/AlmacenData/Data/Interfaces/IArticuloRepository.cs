da1c31ef7048bb8019f5f561d8101ea7f7295926
Tomás Rago <412012@tecnicatura.frc.utn.edu.ar>
2024-09-17 12:14:13 -0300

    Tarea 2

---
+++ tarea2/AlmacenData/Data/Interfaces/IArticuloRepository.cs
@@ -0,0 +1,18 @@
+﻿using Almacen.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Almacen.Data.Interfaces
+{
+    public interface IArticuloRepository
+    {
+        void Create(Articulo a);
+        void Update(Articulo a);
+        void Delete(int id);
+        IEnumerable<Articulo> GetAll();
+        
+    }
+}
